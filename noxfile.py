0d162ba9f332a274397eec513b181fe0542e0292
skshetry <18718008+skshetry@users.noreply.github.com>
2025-04-28 16:31:43 +0545

    drop support for Python 3.8

--- noxfile.py
+++ noxfile.py
@@ -18,13 +18,11 @@ def docs(session: nox.Session) -> None:
 
 @nox.session(
     python=[
-        "3.8",
         "3.9",
         "3.10",
         "3.11",
         "3.12",
         "3.13",
-        "pypy3.8",
         "pypy3.9",
         "pypy3.10",
     ]
